f4acd529fb58e4da3e21fd87fc7dc9029ff6f83f
Francisco Juan <juansoussev@gmail.com>
2024-02-17 22:40:31 -0300

    [add] creating a menu button

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,7 +1,10 @@
+"use client"
+import { useState } from 'react';
 import styles from './links.module.css';
 import NavLink from "./navLink/navLink";
 
 const Links = () => {
+  const [open, setOpen] = useState(false);
 
   const links = [
     {
@@ -26,9 +29,37 @@ const Links = () => {
     },
   ]
 
-  return (links.map(link => (
-        <NavLink key={link.id} link={link} />
-      ))
+  const session = true;
+  const isAdmin = true;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map(link => (
+          <NavLink key={link.id} link={link} />
+        ))}
+        {
+          session ?
+            (
+              <>
+                {isAdmin && <NavLink key='Login' link={{ title: 'Admin', path: '/admin' }} />}
+                <button className={styles.logout} type='button'>Logout</button>
+              </>)
+            : (
+              <NavLink key='Login' link={{ title: 'Login', path: '/login' }} />
+            )
+        }
+      </div>
+      <button>Menu</button>
+      {
+        open && 
+        <div className={styles.mobileLinks}>
+          {
+            links.map(link => <NavLink key={link.id} link={link} />)
+          }
+        </div>
+      }
+    </div>
   )
 }
 
